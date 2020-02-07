56456a281d6fcc909ac404f46a7ec518a17c281e
Cedric <paladicecorporation@gmail.com>
2020-02-07 21:24:15 +0100

    Code propre

--- js/script.js
+++ js/script.js
@@ -1,160 +1,179 @@
-var espaceGrille =[
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 2, 2, 2, 2, 2, 2, 2, 2, 0, 2, 2, 2, 2, 2, 2, 2, 2, 0],
-    [0, 2, 0, 0, 2, 0, 0, 0, 2, 0, 2, 0, 0, 0, 2, 0, 0, 2, 0],
-    [0, 2, 0, 0, 2, 0, 0, 0, 2, 0, 2, 0, 0, 0, 2, 0, 0, 2, 0],
-    [0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0],
-    [0, 2, 0, 0, 2, 0, 2, 0, 0, 0, 0, 0, 2, 0, 2, 0, 0, 2, 0],
-    [0, 2, 2, 2, 2, 0, 2, 2, 2, 0, 2, 2, 2, 0, 2, 2, 2, 2, 0],
-    [0, 0, 0, 0, 2, 0, 0, 0, 2, 0, 2, 0, 0, 0, 2, 0, 0, 0, 0],
-    [0, 1, 1, 0, 2, 0, 2, 2, 2, 2, 2, 2, 2, 0, 2, 0, 1, 1, 0],
-    [0, 0, 0, 0, 2, 0, 2, 0, 0, 2, 0, 0, 2, 0, 2, 0, 0, 0, 0],
-    [2, 2, 2, 2, 2, 2, 2, 0, 2, 2, 2, 0, 2, 2, 2, 2, 2, 2, 2],
-    [0, 0, 0, 0, 2, 0, 2, 0, 0, 2, 0, 0, 2, 0, 2, 0, 0, 0, 0],
-    [0, 1, 1, 0, 2, 0, 2, 2, 2, 2, 2, 2, 2, 0, 2, 0, 1, 1, 0],
-    [0, 0, 0, 0, 2, 0, 2, 0, 0, 0, 0, 0, 2, 0, 2, 0, 0, 0, 0],
-    [0, 2, 2, 2, 2, 2, 2, 2, 2, 0, 2, 2, 2, 2, 2, 2, 2, 2, 0],
-    [0, 2, 0, 0, 2, 0, 0, 0, 2, 0, 2, 0, 0, 0, 2, 0, 0, 2, 0],
-    [0, 2, 2, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0, 2, 2, 0],
-    [0, 0, 2, 0, 2, 0, 2, 0, 0, 0, 0, 0, 2, 0, 2, 0, 2, 0, 0],
-    [0, 2, 2, 2, 2, 0, 2, 2, 2, 0, 2, 2, 2, 0, 2, 2, 2, 2, 0],
-    [0, 2, 0, 0, 0, 0, 0, 0, 2, 0, 2, 0, 0, 0, 0, 0, 0, 2, 0],
-    [0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-];
-
-function viderGrille()
+function jeu()
 {
+    // VARIABLE DECLARER
+
+    var espaceGrille =[
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 2, 2, 2, 2, 2, 2, 2, 2, 0, 2, 2, 2, 2, 2, 2, 2, 2, 0],
+        [0, 2, 0, 0, 2, 0, 0, 0, 2, 0, 2, 0, 0, 0, 2, 0, 0, 2, 0],
+        [0, 2, 0, 0, 2, 0, 0, 0, 2, 0, 2, 0, 0, 0, 2, 0, 0, 2, 0],
+        [0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0],
+        [0, 2, 0, 0, 2, 0, 2, 0, 0, 0, 0, 0, 2, 0, 2, 0, 0, 2, 0],
+        [0, 2, 2, 2, 2, 0, 2, 2, 2, 0, 2, 2, 2, 0, 2, 2, 2, 2, 0],
+        [0, 0, 0, 0, 2, 0, 0, 0, 2, 0, 2, 0, 0, 0, 2, 0, 0, 0, 0],
+        [0, 1, 1, 0, 2, 0, 2, 2, 2, 2, 2, 2, 2, 0, 2, 0, 1, 1, 0],
+        [0, 0, 0, 0, 2, 0, 2, 0, 0, 2, 0, 0, 2, 0, 2, 0, 0, 0, 0],
+        [2, 2, 2, 2, 2, 2, 2, 0, 2, 2, 2, 0, 2, 2, 2, 2, 2, 2, 2],
+        [0, 0, 0, 0, 2, 0, 2, 0, 0, 2, 0, 0, 2, 0, 2, 0, 0, 0, 0],
+        [0, 1, 1, 0, 2, 0, 2, 2, 2, 2, 2, 2, 2, 0, 2, 0, 1, 1, 0],
+        [0, 0, 0, 0, 2, 0, 2, 0, 0, 0, 0, 0, 2, 0, 2, 0, 0, 0, 0],
+        [0, 2, 2, 2, 2, 2, 2, 2, 2, 0, 2, 2, 2, 2, 2, 2, 2, 2, 0],
+        [0, 2, 0, 0, 2, 0, 0, 0, 2, 0, 2, 0, 0, 0, 2, 0, 0, 2, 0],
+        [0, 2, 2, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0, 2, 2, 0],
+        [0, 0, 2, 0, 2, 0, 2, 0, 0, 0, 0, 0, 2, 0, 2, 0, 2, 0, 0],
+        [0, 2, 2, 2, 2, 0, 2, 2, 2, 0, 2, 2, 2, 0, 2, 2, 2, 2, 0],
+        [0, 2, 0, 0, 0, 0, 0, 0, 2, 0, 2, 0, 0, 0, 0, 0, 0, 2, 0],
+        [0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    ];
+    var score = 0;
+    var affichageScore = document.querySelector('h1');
+    var grille = document.querySelector("#grille");
+    var pacman = {
+        x : 5,
+        y : 2,
+        direction : 0
+    }
+
+    // FONCTION DECLARER
+
+    function viderGrille()
+    {
         grille.innerHTML = "";
-}
-function initGrille()
-{
-    /* SELECTION DE LA GRILLE */
-    
-    /* VIDER LA GRILLE */
-    viderGrille();
+    }
 
-    /* CREATION DE LA GRILLE */
-    grille.style.display = "grid";
-    grille.style.gridTemplateRows = "repeat(22, 40px)";
-    grille.style.gridTemplateColumns = "repeat(19, 40px)";
-    let i = 0;
-    let y = 0;
-    
-    /* PARCOURIR LE TABLEAU ET REMPLIR LA GRILLE */
-    while(i < espaceGrille.length)
-    {
-        y = 0;
-        while(y < espaceGrille[i].length)
+    function initGrille()
+    {   
+        /* VIDER LA GRILLE */
+        viderGrille();
+
+        /* CREATION DE LA GRILLE */
+        grille.style.display = "grid";
+        grille.style.gridTemplateRows = "repeat(22, 40px)";
+        grille.style.gridTemplateColumns = "repeat(19, 40px)";
+        let i = 0;
+        let y = 0;
+        
+        /* PARCOURIR LE TABLEAU ET REMPLIR LA GRILLE */
+        while(i < espaceGrille.length)
         {
-            if(espaceGrille[i][y] == 0)
+            y = 0;
+            while(y < espaceGrille[i].length)
             {
-                let mur = document.createElement('div');
-                mur.classList.add("mur")
-                mur.style.gridColumn = (+y) + 1;
-                mur.style.gridRow = (+i) + 1;
-                grille.appendChild(mur);
+                if(espaceGrille[i][y] == 0)
+                {
+                    let mur = document.createElement('div');
+                    mur.classList.add("mur")
+                    mur.style.gridColumn = (+y) + 1;
+                    mur.style.gridRow = (+i) + 1;
+                    grille.appendChild(mur);
+                }
+                if(espaceGrille[i][y] == 1)
+                {
+                    let sol = document.createElement('div');
+                    sol.classList.add("sol");
+                    sol.style.gridColumn = (+y) + 1;
+                    sol.style.gridRow = (+i) + 1;
+                    grille.appendChild(sol);
+                }
+                if(espaceGrille[i][y] == 2)
+                {
+                    let bonbon = document.createElement('div');
+                    bonbon.classList.add("bonbon");
+                    bonbon.style.gridColumn = (+y) + 1;
+                    bonbon.style.gridRow = (+i) + 1;
+                    grille.appendChild(bonbon);
+                }
+                y++;
             }
-            if(espaceGrille[i][y] == 1)
-            {
-                let sol = document.createElement('div');
-                sol.classList.add("sol");
-                sol.style.gridColumn = (+y) + 1;
-                sol.style.gridRow = (+i) + 1;
-                grille.appendChild(sol);
-            }
-            if(espaceGrille[i][y] == 2)
-            {
-                let bonbon = document.createElement('div');
-                bonbon.classList.add("bonbon");
-                bonbon.style.gridColumn = (+y) + 1;
-                bonbon.style.gridRow = (+i) + 1;
-                grille.appendChild(bonbon);
-            }
-            y++;
+            i++;
         }
-        i++;
     }
-}
-var score = 0;
-var affichageScore = document.querySelector('h1');
 
-function pacmanBouge(magrille)
-{
-    document.querySelector('body').addEventListener('keydown', function () {
-        /*console.log(event.key);*/
-        var touche = window.event ? event.keyCode : event.which;
-        if (touche == 38) {
-            pacman.direction = 1;
+    function pacmanBouge(magrille)
+    {
+        /* RECUPERE LES TOUCHES DU CLAVIER */
+        document.querySelector('body').addEventListener('keydown', function () 
+        {
+            var touche = window.event ? event.keyCode : event.which;
+            if (touche == 90) {
+                pacman.direction = 1;
+            }
+            if (touche == 83) {
+                pacman.direction = 2;
+            }
+            if (touche == 69) {
+                pacman.direction = 3;
+            }
+            if (touche == 65) {
+                pacman.direction = 4;
+            }
+        });
+        
+        /* FAIT AVANCER LE PERSONNAGE */
+        if(pacman.direction == 1)
+        {
+            pacman.y--;
         }
-        if (touche == 40) {
-            pacman.direction = 2;
+        else if(pacman.direction == 2)
+        {
+            pacman.y++;
         }
-        if (touche == 39) {
-            pacman.direction = 3;
+        else if(pacman.direction == 3)
+        {
+            pacman.x++;
         }
-        if (touche == 37) {
-            pacman.direction = 4;
+        else if(pacman.direction == 4)
+        {
+            pacman.x--;
         }
-    });
     
-    if(pacman.direction == 1)
-    {
-        pacman.y--;
-        
-    }
-    if(pacman.direction == 2)
-    {
-        pacman.y++;
-    }
-    if(pacman.direction == 3)
-    {
-        pacman.x++;
-    }
-    if(pacman.direction == 4)
-    {
-        pacman.x--;
-    }
-    if(espaceGrille[pacman.y - 1][pacman.x - 1] == 0 && pacman.direction == 1)
-    {
-        pacman.y++;
-    }
-    if(espaceGrille[pacman.y - 1][pacman.x - 1] == 0 && pacman.direction == 2)
-    {
-        pacman.y--;
-    }
-    if(espaceGrille[pacman.y - 1][pacman.x - 1] == 0 && pacman.direction == 3)
-    {
-        pacman.x--;
-    }
-    if(espaceGrille[pacman.y - 1][pacman.x - 1] == 0 && pacman.direction == 4)
-    {
-        pacman.x++;
-    }
-    if(espaceGrille[pacman.y - 1][pacman.x - 1] == 2)
-    {
-        espaceGrille[pacman.y - 1][pacman.x - 1] = 1;
-        
-        score++;
-        affichageScore.textContent = "Votre score : " + score;
-    }
-    let pacmanEmplacement = document.createElement('div');
-    pacmanEmplacement.classList.add("pacman");
-    pacmanEmplacement.style.gridRow = pacman.y;
-    pacmanEmplacement.style.gridColumn = pacman.x;
-    magrille.appendChild(pacmanEmplacement);
-}
+        /* DETECTE SI IL Y A UN MUR */
+        if(espaceGrille[pacman.y - 1][pacman.x - 1] == 0 && pacman.direction == 1)
+        {
+            pacman.y++;
+        }
+        else if(espaceGrille[pacman.y - 1][pacman.x - 1] == 0 && pacman.direction == 2)
+        {
+            pacman.y--;
+        }
+        else if(espaceGrille[pacman.y - 1][pacman.x - 1] == 0 && pacman.direction == 3)
+        {
+            pacman.x--;
+        }
+        else if(espaceGrille[pacman.y - 1][pacman.x - 1] == 0 && pacman.direction == 4)
+        {
+            pacman.x++;
+        }
 
-var pacman = {
-    x : 5,
-    y : 2,
-    direction : 0
-}
-var grille = document.querySelector("#grille");
+        /* DETECTE SI IL Y A DES PIECES */
+        if(espaceGrille[pacman.y - 1][pacman.x - 1] == 2)
+        {
+            espaceGrille[pacman.y - 1][pacman.x - 1] = 1;
+            score++;
+            affichageScore.textContent = "Votre score : " + score;
+        }
+    
+        /* AFFICHAGE */
+        let pacmanEmplacement = document.createElement('div');
+        pacmanEmplacement.classList.add("pacman");
+        pacmanEmplacement.style.gridRow = pacman.y;
+        pacmanEmplacement.style.gridColumn = pacman.x;
+        magrille.appendChild(pacmanEmplacement);
+    }
 
-function refresh() {  
+    function refresh() 
+    {  
         initGrille();
         pacmanBouge(grille);
         setTimeout(refresh, 500);
+    }
+    
+    // ACTUALISE LE JEU
+
+    refresh();
 }
-refresh();
+
+// LANCE LE JEU
+
+jeu(); 

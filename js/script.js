539a5d32eea1703089689432158c7a7aacd99752
Cedric <paladicecorporation@gmail.com>
2020-02-07 11:48:40 +0100

    Grille terminer

---
+++ js/script.js
@@ -0,0 +1,76 @@
+var espaceGrille =[
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 2, 2, 2, 2, 2, 2, 2, 2, 0, 2, 2, 2, 2, 2, 2, 2, 2, 0],
+    [0, 2, 0, 0, 2, 0, 0, 0, 2, 0, 2, 0, 0, 0, 2, 0, 0, 2, 0],
+    [0, 2, 0, 0, 2, 0, 0, 0, 2, 0, 2, 0, 0, 0, 2, 0, 0, 2, 0],
+    [0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0],
+    [0, 2, 0, 0, 2, 0, 2, 0, 0, 0, 0, 0, 2, 0, 2, 0, 0, 2, 0],
+    [0, 2, 2, 2, 2, 0, 2, 2, 2, 0, 2, 2, 2, 0, 2, 2, 2, 2, 0],
+    [0, 0, 0, 0, 2, 0, 0, 0, 2, 0, 2, 0, 0, 0, 2, 0, 0, 0, 0],
+    [0, 1, 1, 0, 2, 0, 2, 2, 2, 2, 2, 2, 2, 0, 2, 0, 1, 1, 0],
+    [0, 0, 0, 0, 2, 0, 2, 0, 0, 2, 0, 0, 2, 0, 2, 0, 0, 0, 0],
+    [2, 2, 2, 2, 2, 2, 2, 0, 2, 2, 2, 0, 2, 2, 2, 2, 2, 2, 2],
+    [0, 0, 0, 0, 2, 0, 2, 0, 0, 2, 0, 0, 2, 0, 2, 0, 0, 0, 0],
+    [0, 1, 1, 0, 2, 0, 2, 2, 2, 2, 2, 2, 2, 0, 2, 0, 1, 1, 0],
+    [0, 0, 0, 0, 2, 0, 2, 0, 0, 0, 0, 0, 2, 0, 2, 0, 0, 0, 0],
+    [0, 2, 2, 2, 2, 2, 2, 2, 2, 0, 2, 2, 2, 2, 2, 2, 2, 2, 0],
+    [0, 2, 0, 0, 2, 0, 0, 0, 2, 0, 2, 0, 0, 0, 2, 0, 0, 2, 0],
+    [0, 2, 2, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0, 2, 2, 0],
+    [0, 0, 2, 0, 2, 0, 2, 0, 0, 0, 0, 0, 2, 0, 2, 0, 2, 0, 0],
+    [0, 2, 2, 2, 2, 0, 2, 2, 2, 0, 2, 2, 2, 0, 2, 2, 2, 2, 0],
+    [0, 2, 0, 0, 0, 0, 0, 0, 2, 0, 2, 0, 0, 0, 0, 0, 0, 2, 0],
+    [0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+];
+
+function viderGrille(grille)
+{
+        grille.innerHTML = " ";
+}
+
+function initGrille()
+{
+    /* SELECTION DE LA GRILLE */
+    var grille = document.querySelector("#grille");
+
+    /* VIDER LA GRILLE */
+    viderGrille(grille);
+
+    /* CREATION DE LA GRILLE */
+    grille.style.display = "grid";
+    grille.style.gridTemplateRows = "repeat(22, 40px)";
+    grille.style.gridTemplateColumns = "repeat(19, 40px)";
+    let i = 0;
+    let y = 0;
+    
+    /* PARCOURIR LE TABLEAU ET REMPLIR LA GRILLE */
+    while(i < espaceGrille.length)
+    {
+        y = 0;
+        while(y < espaceGrille[i].length)
+        {
+            if(espaceGrille[i][y] == 0)
+            {
+                let mur = document.createElement('div');
+                mur.classList.add("mur");
+                grille.appendChild(mur);
+            }
+            if(espaceGrille[i][y] == 1)
+            {
+                let sol = document.createElement('div');
+                sol.classList.add("sol");
+                grille.appendChild(sol);
+            }
+            if(espaceGrille[i][y] == 2)
+            {
+                let bonbon = document.createElement('div');
+                bonbon.classList.add("bonbon");
+                grille.appendChild(bonbon);
+            }
+            y++;
+        }
+        i++;
+    }
+}
+
+initGrille();
